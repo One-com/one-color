One-com/one-color

commit 03950f4be52fbae81251283e270e7e4616af099c

Node check code so it doesn't throw an error in the browser console

lib/one/color/_all.js
@@ -3,6 +3,6 @@ one.include('js:one/color/HSV.js');
 one.include('js:one/color/HSL.js');
 //one.include('js:one/color/CMYK.js');
 
-if (module) {
-	module.exports = one.color;
+if (typeof exports !== 'undefined') {
+	exports = one.color;
 }

commit ef2c2d7db4d30775a49f0f6cb41b8d611465e776

Fixed regression introduced in 03950f4

lib/one/color/_all.js
@@ -3,6 +3,6 @@ one.include('js:one/color/HSV.js');
 one.include('js:one/color/HSL.js');
 //one.include('js:one/color/CMYK.js');
 
-if (typeof exports !== 'undefined') {
-	exports = one.color;
+if (typeof module !== 'undefined') {
+	module.exports = one.color;
 }