6a820ad4119dafc1073f48fda12b34febfb9ebe0
Peter Müller <munter@fumle.dk>
2012-01-14 20:13:48 +0100

    More API tests

--- test/color-test.js
+++ test/color-test.js
@@ -19,28 +19,80 @@ var Color = require("../one-color-debug.js"),
             channels: ['cyan', 'magenta', 'yellow', 'black', 'alpha']
         }*/
     ];
-var suite = vows.describe('Color');
-var clr = new Color.RGB(0, 0, 0, 1);
-var batch = {}
-spaces.forEach(function(space) {
-    var context = {
-        topic: space.name
-    };
+var colorChannels = [];
+var colorSpaces = spaces.map(function (item) {
+    colorChannels = colorChannels.concat(item.channels);
+    return item.name;
+});
 
-    context['is a function'] = function (spaceName) {
+var suite = vows.describe('Color');
+var batch = {};
+
+spaces.forEach(function (space) {
+    var clr,
+        context = {
+            topic: space.name
+        };
+
+    context['is a funtion'] = function (spaceName) {
         assert.isFunction(Color[spaceName]);
     };
-    context['has conversion method'] = function (spaceName) {
-        assert.isFunction(clr[spaceName.toLowerCase()]);
-        assert.isTrue(clr[spaceName.toLowerCase()]().isColor);
+    context['can be constructed'] = function (spaceName) {
+        clr = new Color[spaceName](0, 0, 0, 0);
+        assert.isObject(clr);
+    };
+    context['is Color'] = function (spaceName) {
+        assert.isTrue(clr.isColor);
     };
 
+    colorSpaces.forEach(function (item) {
+        context['has ' + item + ' conversion method'] = function (spaceName) {
+            assert.isFunction(clr[item.toLowerCase()]);
+        };
+    });
+    colorSpaces.forEach(function (item) {
+        context[item + ' conversion'] = function (spaceName) {
+            assert.isTrue(clr[item.toLowerCase()]().isColor);
+        };
+    });
+
+    colorSpaces.forEach(function (item) {
+        context['has equals method'] = function (spaceName) {
+            assert.isFunction(clr.equals);
+        };
+    });
+    colorSpaces.forEach(function (item) {
+        context['equals'] = function (spaceName) {
+            assert.isTrue(clr.equals(new Color[spaceName](0, 0, 0, 0)));
+        };
+    });
+
     var chans = {
-        topic: new Color[space.name](0, 0, 0, 1)
+        topic: new Color[space.name](0, 0, 0, 0)
     }
     space.channels.forEach(function(channel) {
-        chans['has ' + channel + ' getter/setter'] = function (color) {
+        chans[channel + ' getter/setter existance'] = function (color) {
             assert.isFunction(color[channel]);
+        };
+        chans[channel + ' getter'] = function (color) {
+            assert.equal(color[channel](), 0);
+        };
+        chans[channel + ' setter'] = function (color) {
+            assert.equal(color[channel](0)[channel](), 0);
+            assert.equal(color[channel](0.5)[channel](), 0.5);
+            if (channel === 'hue') {
+                // Hue is considered a circle, and thus has periodic boundary conditions
+                assert.equal(color[channel](1)[channel](), 0);
+                assert.equal(color[channel](-0.1)[channel](), 0.9);
+                assert.equal(color[channel](1.5)[channel](), 0.5);
+            } else {
+                assert.equal(color[channel](1)[channel](), 1);
+                assert.equal(color[channel](-0.1)[channel](), 0);
+                assert.equal(color[channel](1.1)[channel](), 1);
+            }
+        };
+        chans[channel + ' adjustment'] = function (color) {
+            assert.equal(color[channel](0.5, true)[channel](), 0.5);
         };
     });
     context['channels'] = chans;
@@ -48,6 +100,15 @@ spaces.forEach(function(space) {
     batch[space.name] = context;
 });
 
+/*
+valueOf: [Function: valueOf],
+hex: [Function],
+css: [Function],
+cssa: [Function],
+toJSON: [Function],
+toString: [Function]
+*/
+
 suite.addBatch({
-    'Color API': batch
+    'API': batch
 }).export(module);
