cdf24deafc208a750f7f2913fc43bbd71bc0e09a
Andreas Lind <andreas@one.com>
2016-03-15 00:26:45 +0100

    Actually check in the converted conversion test.

--- test/conversion.js
+++ test/conversion.js
@@ -1,46 +1,22 @@
-var vows = require('vows'),
-    assert = require('assert'),
-    namedColorSamples = require('./samples'),
-    spaces = [
-        'rgb',
-        'hsl',
-        'hsv'
-    ];
+var expect = require('unexpected');
+var namedColorSamples = require('./samples');
+var color = require('../');
+var spaces = [
+    'rgb',
+    'hsl',
+    'hsv',
+    'cmyk'
+];
 
-function createTest(bundleFileName) {
-    var Color = require(bundleFileName),
-        batch = {};
-
+describe('conversion', function () {
     Object.keys(namedColorSamples).forEach(function (namedColor) {
-        var sub = batch[namedColor + ': ' + namedColorSamples[namedColor]] = {
-            topic: Color(namedColorSamples[namedColor])
-        };
-        spaces.forEach(function (space) {
-            sub[space.toUpperCase() + ' hex string comparison'] = function (topic) {
-                assert.equal(topic[space]().hex(), topic.hex());
-            };
-            /*
-            sub[space.toUpperCase() + ' strict comparison'] = function (topic) {
-                assert.ok(topic.equals(topic[space]()));
-            };*/
+        describe('with named color sample ' + namedColor, function () {
+            var instance = color(namedColorSamples[namedColor]);
+            spaces.forEach(function (space) {
+                it('should convert to ' + space, function () {
+                    expect(instance[space]().hex(), 'to equal', instance.hex());
+                });
+            });
         });
     });
-
-    return batch;
-}
-/*
-vows.describe('Color').addBatch({
-    'base, debug': createTest('../one-color-debug'),
-    'base, minified': createTest('../one-color')
-}).export(module);
-*/
-spaces.push(
-    'cmyk'/*,
-    'xyz',
-    'lab'*/
-);
-
-vows.describe('Color-all').addBatch({
-    //'all, debug': createTest('../one-color-all-debug'),
-    'all, minified': createTest('../one-color-all')
-}).export(module);
+});
