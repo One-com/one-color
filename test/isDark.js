d6937fb4d1769e45eb4e37f2a4283a2a2a841e93
Darek Kay <darekkay@gmail.com>
2018-09-13 08:27:21 +0200

    Add luminance, contrast and darkness values
    Add plugins to calculate the luminance, contrast ratio and darkness values for colors.
    This code is based on the [color](https://github.com/Qix-/color/blob/master/index.js) library's code, which unfortunately is not available for browser usage.

---
+++ test/isDark.js
@@ -0,0 +1,15 @@
+var color = require('../');
+
+var expect = require('unexpected').clone();
+
+describe('isDark', function () {
+    it('should return true, if the color is dark', function () {
+        expect(color('black').isDark(), 'to equal', true);
+        expect(color('white').isDark(), 'to equal', false);
+        expect(color('blue').isDark(), 'to equal', true);
+        expect(color('darkgreen').isDark(), 'to equal', true);
+        expect(color('pink').isDark(), 'to equal', false);
+        expect(color('goldenrod').isDark(), 'to equal', false);
+        expect(color('red').isDark(), 'to equal', true);
+    });
+});
